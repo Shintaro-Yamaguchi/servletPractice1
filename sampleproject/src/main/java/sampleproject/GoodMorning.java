05cb65cbe2ea7dfd4d79bf0a8f2092b2ef7d7f40
Shintaro Yamaguchi <Shintaro Yamaguchi@LAPTOP-FH8LBKGF>
2021-06-14 21:28:37 +0900

    6/14(月)更新

--- sampleproject/src/main/java/sampleproject/GoodMorning.java
+++ sampleproject/src/main/java/sampleproject/GoodMorning.java
@@ -1,5 +1,10 @@
 package sampleproject;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,6 +22,30 @@ private static final long serialVersionUID = 1L;
 	request.setAttribute("morning", "GoodMorning");
 //	int num = Integer.parseInt(request.getParameter("num"));
 	String num = request.getParameter("num");
+	String msg="";
+	try {
+	      Class.forName("com.mysql.jdbc.Driver").newInstance();
+	      msg = "ドライバのロードに成功しました";
+	    }catch (ClassNotFoundException e){
+	      msg = "ドライバのロードに失敗しました";
+	    }catch (Exception e){
+	      msg = "ドライバのロードに失敗しました";
+	    }
+	System.out.println(msg);
+	try {
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/itemdb","root","YamaShin5032");
+		String sql = "select itemid, item_name, price from item";
+		PreparedStatement st = con.prepareStatement(sql);
+		ResultSet rs = st.executeQuery();
+		while (rs.next()) {
+			System.out.println(rs.getInt("itemid"));
+		}
+		st.close();
+		con.close();
+	} catch (SQLException e) {
+		// TODO 自動生成された catch ブロック
+		e.printStackTrace();
+	}
 //	request.setAttribute("number",num*10);
 	request.setAttribute("number",num);
 	RequestDispatcher dispatcher = request.getRequestDispatcher(view);	
