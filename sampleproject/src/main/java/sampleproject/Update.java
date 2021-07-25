5cf7440364b5f2fd1c5deafd52739a05ea1219ac
Shintaro Yamaguchi <Shintaro Yamaguchi@LAPTOP-FH8LBKGF>
2021-07-25 13:03:52 +0900

    7/25更新

--- sampleproject/src/main/java/sampleproject/Update.java
+++ sampleproject/src/main/java/sampleproject/Update.java
@@ -1,7 +1,5 @@
 package sampleproject;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,38 +10,39 @@ import javax.servlet.http.HttpServletResponse;
 public class Update extends HttpServlet {
 private static final long serialVersionUID = 1L;
 
-  String value1 = null;
+  String value1;
   String value2 = null;
-  String value3 = null;
+  String value3;
   String value4 = null;
 
   public void doGet(HttpServletRequest request, HttpServletResponse response)
   throws IOException, ServletException
   {
 	String view = "/WEB-INF/view/update.jsp";
-	RequestDispatcher dispatcher = request.getRequestDispatcher(view);	
-	dispatcher.forward(request, response);
+	RequestDispatcher dispatcher = request.getRequestDispatcher(view);
 //	String sql = "select * from contact";
 	
-	List<String> idlist = new ArrayList<>();
-	List<String> usernamelist = new ArrayList<>();
-	List<String> genderlist = new ArrayList<>();
-	List<String> messagelist = new ArrayList<>();
+//	List<String> idlist = new ArrayList<>();
+//	List<String> usernamelist = new ArrayList<>();
+//	List<String> genderlist = new ArrayList<>();
+//	List<String> messagelist = new ArrayList<>();
 	
-	idlist.add(value1);
-	usernamelist.add(value2);
-	genderlist.add(value3);
-	messagelist.add(value4);
+//	idlist.add(value1);
+//	usernamelist.add(value2);
+//	genderlist.add(value3);
+//	messagelist.add(value4);
 	
 //	System.out.println(idlist);
 //	System.out.println(usernamelist);
 //	System.out.println(genderlist);
 //	System.out.println(messagelist);
 	
-	request.setAttribute("id", idlist);
-	request.setAttribute("username", usernamelist);
-	request.setAttribute("gender", genderlist);
-	request.setAttribute("message", messagelist);
+	request.setAttribute("id", value1);
+	request.setAttribute("username", value2);
+	request.setAttribute("gender", value3);
+	request.setAttribute("message", value4);
+	
+	dispatcher.forward(request, response);
 	
   }
   
--- sampleproject/src/main/webapp/WEB-INF/view/update.jsp
+++ sampleproject/src/main/webapp/WEB-INF/view/update.jsp
@@ -1,22 +1,43 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import ="java.util.ArrayList" %>
+<%@ page import = "java.util.List" %>
 <!DOCTYPE html>
 <html>
 	<head>
     <meta charset="utf-8">
     <title>データ更新</title>
     <meta name="description" content="データ更新">
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/sampleStyle.css">
   </head>
 </head>
 <body>
 	<h1>データ更新</h1>
-
-	<% ArrayList<Integer> id = (ArrayList<Integer>) request.getAttribute("id"); %>
-	<% ArrayList<String> username = (ArrayList<String>) request.getAttribute("username"); %>
-	<% ArrayList<Integer> gender = (ArrayList<Integer>) request.getAttribute("gender"); %>
-	<% ArrayList<String> message = (ArrayList<String>) request.getAttribute("message"); %>
 	
-	<%= out.println(id) %>
+	<%= request.getAttribute("id") %>
+	<%= request.getAttribute("username") %>
+	<%= request.getAttribute("gender") %>
+	<%= request.getAttribute("message") %>
+	
+	<form name="contact-form" method="post" action="getcontact" class="contact-form">
+      <dl>
+      	<dt>登録No. <%= request.getAttribute("id") %></dt><br>
+        <dt>名前</dt>
+        <dd><input type="text" name="username" value="<%= request.getAttribute("username") %>"></dd>
+        <dt>性別</dt>
+        <dd>
+	        <input type="radio" name="gender" value="0">男性
+	        <input type="radio" name="gender" value="1">女性
+        </dd>
+        <dt>問合内容</dt>
+        <dd><textarea name="message" class="message"><%= request.getAttribute("message") %></textarea></dd>
+        <dt>添付ファイル</dt>
+        <dd><input type="file" name="file" class="file"></dd>
+      </dl>
+      <div>
+        <input type="submit" value="送信" class="send">
+      </div>
+    </form>
 	
 	
 </body>
